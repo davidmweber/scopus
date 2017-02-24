91d50c1b1eee5bd64cc20e5e2b9fb13916426a77
David Weber <dave@horn.co>
2017-02-24 12:20:00 +0200

    Convert some speed critical loops to while()

--- src/main/scala/za/co/monadic/scopus/ArrayConversion.scala
+++ src/main/scala/za/co/monadic/scopus/ArrayConversion.scala
@@ -11,8 +11,10 @@ object ArrayConversion {
   def byteArrayToShortArray(in: Array[Byte]): Array[Short] = {
     require(in.length % 2 == 0)
     val dest = new Array[Short](in.length / 2)
-    for (i <- dest.indices) {
+    var i    = 0
+    while (i < dest.length) {
       dest(i) = (((in(2 * i + 1) & 0xff) << 8) | (in(2 * i) & 0xFF)).toShort
+      i += 1
     }
     dest
   }
@@ -24,21 +26,25 @@ object ArrayConversion {
     */
   def shortArrayToByteArray(in: Array[Short]): Array[Byte] = {
     val dest = new Array[Byte](in.length * 2)
-    for (i <- in.indices) {
+    var i    = 0
+    while (i < in.length) {
       dest(2 * i) = (in(i) & 0xFF).toByte
       dest(2 * i + 1) = ((in(i) & 0xFF00) >> 8).toByte
+      i += 1
     }
     dest
   }
 
   def floatArrayToByteArray(in: Array[Float]): Array[Byte] = {
     val dest = new Array[Byte](in.length * 4)
-    for (i <- in.indices) {
+    var i    = 0
+    while (i < in.length) {
       val temp = java.lang.Float.floatToIntBits(in(i))
       dest(4 * i) = (temp & 0xFF).toByte
       dest(4 * i + 1) = ((temp & 0xFF00) >> 8).toByte
       dest(4 * i + 2) = ((temp & 0xFF0000) >> 16).toByte
       dest(4 * i + 3) = ((temp & 0xFF000000) >> 24).toByte
+      i += 1
     }
     dest
   }
@@ -46,12 +52,14 @@ object ArrayConversion {
   def byteArrayToFloatArray(in: Array[Byte]): Array[Float] = {
     require(in.length % 4 == 0)
     val dest = new Array[Float](in.length / 4)
-    for (i <- dest.indices) {
+    var i    = 0
+    while (i < dest.length) {
       val temp = ((in(4 * i + 3) & 0xFF) << 24) |
           ((in(4 * i + 2) & 0xFF) << 16) |
           ((in(4 * i + 1) & 0xFF) << 8) |
           (in(4 * i) & 0xFF)
       dest(i) = java.lang.Float.intBitsToFloat(temp)
+      i += 1
     }
     dest
   }
